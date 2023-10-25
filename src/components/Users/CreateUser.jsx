99659b67a807d769ed54d7a57c2ad4fefdbb429d
ANDyouNo <younofl312w@gmail.com>
2023-10-25 14:03:08 +0300

    90

--- src/components/Users/CreateUser.jsx
+++ src/components/Users/CreateUser.jsx
@@ -2,74 +2,65 @@ import styles from "./CreateUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
-import React from "react";
-
+import React, { useRef } from "react";
 
 import { useState } from "react";
 
 const CreateUser = (props) => {
-  const [inputName, setInputName] = useState("");
-  const [inputAge, setInputAge] = useState("");
+  const inputNameRef = useRef(null);
+  const inputAgeRef = useRef(null);
+
+
   const [error, setError] = useState();
-
-  const nameChangeHandler = (event) => {
-    setInputName(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setInputAge(event.target.value);
-  };
 
   const createUserHandler = (event) => {
     event.preventDefault();
-    if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
+    const userInputName = inputNameRef.current.value;
+    const userInputAge = inputAgeRef.current.value;
+    if (userInputName.trim().length === 0 || userInputAge.trim().length === 0) {
       setError({
         title: "Error",
-        message: "Please fill valid Name and Age",  
-      })
+        message: "Please fill valid Name and Age",
+      });
       // if name or age is empty
       return;
     }
-    if (+inputAge < 1) {
+    if (+userInputAge < 1) {
       setError({
         title: "Error",
-        message: "Please fill valid Age",  
-      })
+        message: "Please fill valid Age",
+      });
       // + Проверка на строку
       return;
     }
-    props.onCreateUser(inputName, inputAge);
-    setInputAge("");
-    setInputName("");
+    props.onCreateUser(userInputName, userInputAge);
+    inputNameRef.current.value = ''
+    inputAgeRef.current.value = ''
   };
   const errorHandler = () => {
     setError(false);
-  }
+  };
 
   return (
     <React.Fragment>
-      {error && <ErrorModal onCloseModal={errorHandler} title={error.title} message={error.message} />}
+      {error && (
+        <ErrorModal
+          onCloseModal={errorHandler}
+          title={error.title}
+          message={error.message}
+        />
+      )}
       <Card className={styles.input}>
         <form onSubmit={createUserHandler} action="">
           <label htmlFor="name">Name</label>
-          <input
-            id="name"
-            type="text"
-            onChange={nameChangeHandler}
-            value={inputName}
-          />
+          <input id="name" type="text" ref={inputNameRef} />
           <label htmlFor="age">Age</label>
-          <input
-            id="age"
-            type="number"
-            onChange={ageChangeHandler}
-            value={inputAge}
-          />
+          <input id="age" type="number" ref={inputAgeRef} />
           <Button type="submit">Add user</Button>
         </form>
       </Card>
     </React.Fragment>
   );
-}; 
+};
 
 export default CreateUser;
